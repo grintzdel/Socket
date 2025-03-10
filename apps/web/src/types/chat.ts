a25e2703543a7dddd9b5ed37190037feda15b4bf
grintzdel <mathis.oudin@etu.uca.fr>
2025-03-10 14:54:32 +0100

    :label: Added global chat types

---
+++ apps/web/src/types/chat.ts
@@ -0,0 +1,36 @@
+import type {Socket} from 'socket.io-client'
+
+export type User = {
+    id?: string;
+    username?: string;
+};
+
+export type Message = {
+    id?: string;
+    message: string;
+    sender?: {
+        id: string;
+        username: string;
+    };
+    timestamp?: number;
+};
+
+export type UserContextType = {
+    user: User | null;
+    setUser: (user: User | null) => void;
+};
+
+export type ChatContextType = {
+    messages: Message[];
+    addMessage: (message: Message) => void;
+    typingUsers: string[];
+    setTypingUsers: (users: string[]) => void;
+};
+
+export type SocketHookReturn = {
+    socket: Socket | null;
+    initSocket: () => () => void;
+    sendMessage: (message: string) => void;
+    startTyping: () => void;
+    stopTyping: () => void;
+};
